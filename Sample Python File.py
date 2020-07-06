340807503e178f1827233927e68d119757648615
Iancee <iancee@hotmail.com>
2020-07-05 23:11:52 -0700

    Add files via upload

---
+++ Sample Python File.py
@@ -0,0 +1,15 @@
+#Title of script and general purpose, date created
+import library1
+import library2
+
+#Set global variables
+folder = r"C:\Users\ian.conroy\Desktop\Bay Geo Classes\Python Class\Intro Class"
+
+
+#Use comments to explain what each step of the script is for
+
+#Create a variable holding the string Hello World!
+phrase = 'Hello World!'
+
+#Print the phrase
+print(phrase)
